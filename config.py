92fd79e846b1cb5f7f4f5ae717549c9ab968b74e
cyber35rus <pav.ser35rus@gmail.com>
2026-09-17 12:38:13 +0300

    Update ADMIN_IDS with user guidance comments
    Added comments for user guidance on setting ADMIN_IDS.

--- config.py
+++ config.py
@@ -1,4 +1,7 @@
 import os
 
 BOT_TOKEN = os.environ.get("TELEGRAM_BOT_TOKEN")
-ADMIN_IDS = []  # сюда позже впишем ваш telegram id
+ADMIN_IDS = [
+    # впиши свой telegram id ниже, например:
+    # 123456789,
+]
